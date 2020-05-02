98340a655fae9f5e2791ab5047fe5aa9e2712e00
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-02 00:37:27 -0400

    fixed authentication to check for env username and env account

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,11 +1,14 @@
+//import dotenv from 'dotenv';
 import { Request, Response } from "express";
 import { AuthenticationError, AuthorizationError } from "../errors/errors";
+
+//dotenv.config();
 
 export const adminGuard = (req: Request, resp: Response, next) => {
 
     if (!req.session.principal) {
         resp.status(401).json(new AuthenticationError('No session found! Please login.'));
-    } else if (req.session.principal.role === 'Admin') {
+    } else if (req.session.principal.username === process.env['AU_USERNAME'] && req.session.principal.account_name === process.env['AU_ACCOUNT'] ) {
         next();
     } else {
         resp.status(403).json(new AuthorizationError());
