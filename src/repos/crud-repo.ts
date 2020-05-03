ca3116dfada908cdcfd1dbed52267cbbc0a13d99
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-03 17:44:11 -0400

    Added a type guard for the getId since users can get more than one character

--- src/repos/crud-repo.ts
+++ src/repos/crud-repo.ts
@@ -1,6 +1,6 @@
 export interface CrudRepository<T> {
     getAll(): Promise<T[]>;
-    getById(id: number): Promise<T>;
+    getById(id: number): Promise<T> | Promise<T[]>;
     save(newObj: T): Promise<T>;
     update(updatedObj: T): Promise<boolean>;
     deleteById(id: number): Promise<boolean>;
