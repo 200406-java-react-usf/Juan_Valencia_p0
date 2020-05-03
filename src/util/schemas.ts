7d3e947c86f13fb1a9fb5f876a32589a94b9cf2d
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-03 17:43:05 -0400

    Change schemas to better represent the table names

--- src/util/schemas.ts
+++ src/util/schemas.ts
@@ -16,9 +16,9 @@ export interface CharacterSchema {
 
 export interface StatSchema {
     stat_id: number;
-    user_id: number;
     avg_rank: number;
     avg_char_level: number;
     improved: boolean;
     created_on: string;
+    account_name: string;
 }
