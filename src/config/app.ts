793dc44c82e0ed13e8cae56e9f7f4e05b08568d2
jdvalencia <jdvalencia@users.noreply.github.com>
2020-05-03 17:49:38 -0400

    added a new stat instance

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,22 @@
-import { UserRepository } from "../repos/user-repo";
-import { UserService } from "../services/user-service";
+import { UserRepository } from '../repos/user-repo';
+import { UserService } from '../services/user-service';
+import { CharRepository } from '../repos/char-repo';
+import { CharService } from '../services/char-service';
+import { StatRepository } from '../repos/stat-repo';
+import { StatService } from '../services/stat-service';
 
 const userRepo = new UserRepository();
 const userService = new UserService(userRepo);
 
+const charRepo = new CharRepository();
+const charService = new CharService(charRepo);
+
+const statRepo = new StatRepository();
+const statService = new StatService(statRepo);
+
 
 export default {
-    userService
-}
+    userService,
+    charService,
+    statService
+};
